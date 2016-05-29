eb173519b30a0a2a556ca3fb4df97e729451e3d5
lsta <alastair.s@gmail.com>
2016-05-30 08:32:16 +1200

    1st go

---
+++ Windows_client_audit/rubbish.ps1
@@ -0,0 +1,22 @@
+﻿
+#add local accounts
+$computername = $env:computername   # place computername here for remote access
+$desc = 'Automatically created local admin account'
+
+$computer = [ADSI]"WinNT://$computername,computer"
+$user = $computer.Create("user", $username)
+$user.SetPassword($password)
+$user.Setinfo()
+$user.description = $desc
+$user.setinfo()
+$user.UserFlags = 65536
+$user.SetInfo()
+$group = [ADSI]("WinNT://$computername/administrators,group")
+$group.add("WinNT://$username,user")
+
+
+wmic useraccount where "name='ITCOAdmin'" set passwordexpires=FALSE
+net localgroup "Administrators" ITCOAdmin /add
+
+#windows 7
+NetSh getAdvfirewall set allrprofiles state on
